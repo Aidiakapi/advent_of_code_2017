69c220848b89c9fa25d25080dbaa8011f8906653
Aidiakapi <aidiakapi@gmail.com>
2022-10-18 10:55:42 +0200

    Implement day05.

---
+++ src/day05.rs
@@ -0,0 +1,42 @@
+framework::day!(05, true, parse => pt1, pt2);
+
+fn pts<F: Fn(i32) -> i32>(input: &[i32], remap: F) -> usize {
+    let mut input = input.to_vec();
+    let mut counter = 0;
+    let mut ip = 0i32;
+    loop {
+        match ip.try_into().ok().and_then(|idx: usize| input.get_mut(idx)) {
+            Some(v) => {
+                counter += 1;
+                ip += *v;
+                *v = remap(*v);
+            }
+            None => break counter,
+        }
+    }
+}
+
+fn pt1(input: &[i32]) -> usize {
+    pts(input, |n| n + 1)
+}
+
+fn pt2(input: &[i32]) -> usize {
+    pts(input, |n| if n >= 3 { n - 1 } else { n + 1 })
+}
+
+fn parse(input: &[u8]) -> Result<Vec<i32>> {
+    use parsers::*;
+    number::<i32>().sep_by(token(b'\n')).execute(input)
+}
+
+tests! {
+    const EXAMPLE: &'static [u8] = b"\
+0
+3
+0
+1
+-3";
+
+    test_pt!(parse, pt1, EXAMPLE => 5);
+    test_pt!(parse, pt2, EXAMPLE => 10);
+}
--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,5 @@ framework::main!(
     day02,
     day03,
     day04,
+    day05,
 );
